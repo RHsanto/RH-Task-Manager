1ef9845872b2438f26242695ce8014fe21501ca7
RHsanto <rakibulsanto789@gmail.com>
2023-01-08 00:24:13 +0600

    first commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="main_section">
+      <header className="shadow-2xl p-4 ">
+        <h1 className="text-3xl text-center">RH Task Manager </h1>
+      </header>
+      <div className="md:container md:mx-auto">
+        <div className=" grid md:grid-cols-4 grid-cols-1 gap-4 mt-20">
+          <div id="todo_card" className="bg-slate-200">
+            <div
+              id="todo_head"
+              className="bg-indigo-700 rounded
+             text-white p-4 flex justify-between items-center"
+            >
+              <h1>TO DO</h1>
+              <button className="text-3xl border rounded-lg px-3">+</button>
+            </div>
+            <div className=""></div>
+          </div>
+          <div id="in_process" className="">
+            <div
+              id="in-process_head"
+              className="bg-cyan-400 rounded
+             text-white p-4 flex justify-between items-center"
+            >
+              <h1>IN PROGRESS</h1>
+              <button className="text-3xl border rounded-lg px-3">+</button>
+            </div>
+          </div>
+          <div id="review_card" className="">
+            <div
+              id="review_head"
+              className="bg-amber-400	 rounded
+             text-white p-4 flex justify-between items-center"
+            >
+              <h1>REVIEW</h1>
+              <button className="text-3xl border rounded-lg px-3">+</button>
+            </div>
+          </div>
+          <div id="done-card" className="">
+            <div
+              id="done-head"
+              className="bg-lime-500	 rounded
+             text-white p-4 flex justify-between items-center"
+            >
+              <h1>DONE</h1>
+              <button className="text-3xl border rounded-lg px-3">+</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
